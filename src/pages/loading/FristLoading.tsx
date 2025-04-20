5cb07ba4b59536b1097a7aed9d3c936941932d9f
nitish <nitishlovecode@gmail.com>
2025-04-20 19:11:17 +0530

    added animation to loading pae

---
+++ src/pages/loading/FristLoading.tsx
@@ -0,0 +1,43 @@
+import { Fragment, useEffect, useState } from 'react';
+import logo from "../../assets/logo/ilovedsalogo.png";
+
+function FristLoading() {
+  const [moveLeft, setMoveLeft] = useState<boolean>(false);
+  const [loveMove, setLoveMove] = useState<boolean>(false)
+
+
+  useEffect(() =>{
+    const timer = setTimeout(() => {
+      setMoveLeft(true);
+    }, 3000);
+
+    const loveTimer = setTimeout(() => {
+      setLoveMove(true);
+    }, 4000);
+
+    return(()=> {
+      clearTimeout(timer);
+      clearTimeout(loveTimer);
+    });
+  }, [])
+
+
+  // absolute bottom-1 left-2
+
+  return (
+    <Fragment>
+        <div>
+          <div className={`bg-gradient-to-r from-amber-500 to-pink-500 h-screen flex justify-center
+            transition-all duration-900 ${moveLeft ? "w-[20vw]" : "w-screen items-center"}`}>
+              <div className='relative'>
+                  <img className='h-14' src={logo} alt="ilovedsa.com" />
+                  <div style={{fontSize: "42px"}} className={`absolute ${loveMove ? "-top-2" : "bottom-1 animate-spin -top-2"} left-2`}>❤️</div>
+              </div>
+          </div>
+        </div>
+
+    </Fragment>
+  )
+}
+
+export default FristLoading
