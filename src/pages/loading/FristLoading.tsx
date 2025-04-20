e298250e201d687c7d73792de49a1aea20f45cb4
nitish <nitishlovecode@gmail.com>
2025-04-20 20:11:53 +0530

    changes made for sidebar

--- src/pages/loading/FristLoading.tsx
+++ src/pages/loading/FristLoading.tsx
@@ -1,7 +1,7 @@
 import { Fragment, useEffect, useState } from 'react';
 import logo from "../../assets/logo/ilovedsalogo.png";
 
-function FristLoading() {
+export default   function FristLoading() {
   const [moveLeft, setMoveLeft] = useState<boolean>(false);
   const [loveMove, setLoveMove] = useState<boolean>(false)
 
@@ -26,36 +26,85 @@ function FristLoading() {
 
   return (
     <Fragment>
-        <div>
-          <div className={`bg-gradient-to-r from-amber-500 to-pink-500 h-screen flex justify-center
-            transition-all duration-900 ${moveLeft ? "w-[20vw]" : "w-screen items-center"}`}>
-              <div className='relative'>
-                  <img className='h-14' src={logo} alt="ilovedsa.com" />
-                  <div style={{fontSize: "42px"}} className={`absolute ${loveMove ? "-top-2" : "bottom-1 animate-spin -top-2"} left-2`}>❤️</div>
-              </div>
-          </div>
-        </div>
 
-    </Fragment>
-  )
-}
+      <div className='flex'>
 
-export default FristLoading
-import { Fragment } from 'react';
-import logo from "../../assets/logo/ilovedsalogo.png";
+        {/* Right side bar */}
 
-function FristLoading() {
-  return (
-    <Fragment>
-        <div className='bg-gradient-to-r from-amber-500 to-pink-500 h-screen w-screen flex items-center justify-center'>
-            <div className='relative'>
-                <img className='h-14' src={logo} alt="ilovedsa.com" />
-                <div style={{fontSize: "42px"}} className='absolute bottom-1 left-2 animate-spin'>❤️</div>
+        <div className=''>
+
+            <div className={`bg-gradient-to-r from-amber-500 to-pink-500 flex justify-center
+              transition-all duration-900 ${moveLeft ? "w-[25vw] h-[10vh]" : "w-screen h-screen items-center"}`}>
+                <div className='relative'>
+                    <img className='h-14' src={logo} alt="ilovedsa.com" />
+                    <div style={{fontSize: "42px"}} className={`absolute ${loveMove ? "-top-2" : "bottom-1 animate-spin -top-2"} left-2`}>❤️</div>
+                </div>
             </div>
+
+            {/* ---------- menue bar ----------*/}
+
+            {
+              loveMove && 
+              <div className='bg-red-50 overflow-y-scroll h-[90vh]'>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+                <h3>Comming soon Lorem ipsum.</h3>
+              </div>
+            }
+
         </div>
+
+        
+
+
+        {/* // Right side bar */}
+
+        <div className='w-[75vw] h-screen bg-gray-100'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi est inventore minima harum excepturi ex culpa cupiditate, a quibusdam blanditiis ab iure quos commodi nihil, debitis perspiciatis et assumenda error.
+        </div>
+
+      </div>
 
     </Fragment>
   )
 }
-
-export default FristLoading
