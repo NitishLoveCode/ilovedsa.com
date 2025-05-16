5a6973ed13ab8246c711b067afc0380b663ed605
nitish <nitishlovecode@gmail.com>
2025-05-17 00:01:56 +0530

    Bug fixed from question set

--- src/constants/QuestionBank/interViewQuestion/questionSet1.ts
+++ src/constants/QuestionBank/interViewQuestion/questionSet1.ts
@@ -5,7 +5,7 @@ export const questionSet1 = [
     description:
       "Write a function to reverse string without using any inbuild function javascript.\n\n for Example:\n\n Input: 'Hello' \n\n Output: 'olleH' ",
     starterCode:
-      "function reverseString(str) {\n  // Hey coader, write your code from here. \n}\n\n reverseString('hello');",
+      "function reverseString(str) {\n  // Hey coder, write your code from here. \n}\n\n console.log(reverseString('hello'));",
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const questionSet1 = [
     description:
       "Write a function to reverse string with using inbuild function javascript.\n\n for Example:\n\n Input: 'Hello' \n\n Output: 'olleH' ",
     starterCode:
-      "function reverseString(str) {\n  // Hey coader, write your code from here. \n}\n\n reverseString('hello');",
+      "function reverseString(str) {\n  // Hey coder, write your code from here. \n}\n\n console.log(reverseString('hello'));",
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const questionSet1 = [
     description:
       "Write a function to check if a string is a palindrome without using any inbuild function in JavaScript.\n\n A palindrome is a word that reads the same backward as forward.\n\n For Example:\n\n Input: 'madam' \n\n Output: true \n\n Input: 'hello'\n\n Output: false",
     starterCode:
-      "function isPalindrome(str) {\n  // Hey coader, write your code from here. \n}\n\n isPalindrome('madam');",
+      "function isPalindrome(str) {\n  // Hey coder, write your code from here. \n}\n\n console.log(isPalindrome('madam'));",
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ export const questionSet1 = [
     description:
       "Write a function to check if a string is a palindrome using JavaScript inbuild functions.\n\n A palindrome is a word that reads the same backward as forward.\n\n For Example:\n\n Input: 'madam' \n\n Output: true \n\n Input: 'hello'\n\n Output: false",
     starterCode:
-      "function isPalindrome(str) {\n  // Hey coader, write your code from here. \n}\n\n isPalindrome('madam');",
+      "function isPalindrome(str) {\n  // Hey coder, write your code from here. \n}\n\n console.log(isPalindrome('madam'));",
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ export const questionSet1 = [
     description:
       "Write a function to find the factorial of a given number.\n\n The factorial of a non-negative integer `n` is the product of all positive integers less than or equal to `n`.\n\n For Example:\n\n Input: 5\n\n Output: 120 (because 5*4*3*2*1 = 120)\n\n Input: 0\n\n Output: 1 (by definition, factorial of 0 is 1)",
     starterCode:
-      "function findFactorial(n) {\n  // Hey coader, write your code from here. \n}\n\n findFactorial(5);",
+      "function findFactorial(n) {\n  // Hey coder, write your code from here. \n}\n\n console.log(findFactorial(5));",
   },
   {
     id: 6,
@@ -46,7 +46,7 @@ export const questionSet1 = [
     description:
       "Write a function to find the largest number in an array without using any inbuild functions like Math.max().\n\n For Example:\n\n Input: [3, 7, 2, 9, 4]\n\n Output: 9",
     starterCode:
-      "function findLargestNumber(arr) {\n  // Hey coader, write your code from here. \n}\n\n findLargestNumber([3, 7, 2, 9, 4]);",
+      "function findLargestNumber(arr) {\n  // Hey coder, write your code from here. \n}\n\n console.log(findLargestNumber([3, 7, 2, 9, 4]));",
   },
   {
     id: 7,
@@ -54,7 +54,7 @@ export const questionSet1 = [
     description:
       "Write a function to find the largest number in an array using inbuild functions like Math.max().\n\n For Example:\n\n Input: [3, 7, 2, 9, 4]\n\n Output: 9",
     starterCode:
-      "function findLargestNumber(arr) {\n  // Hey coader, write your code from here. \n}\n\n findLargestNumber([3, 7, 2, 9, 4]);",
+      "function findLargestNumber(arr) {\n  // Hey coder, write your code from here. \n}\n\n console.log(findLargestNumber([3, 7, 2, 9, 4]));",
   },
   {
     id: 8,
@@ -62,7 +62,7 @@ export const questionSet1 = [
     description:
       "Write a function to remove duplicate values from an array without using any inbuild functions like Set, filter, etc.\n\n For Example:\n\n Input: [1, 2, 2, 3, 4, 4, 5]\n\n Output: [1, 2, 3, 4, 5]",
     starterCode:
-      "function removeDuplicates(arr) {\n  // Hey coader, write your code from here. \n}\n\n console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));",
+      "function removeDuplicates(arr) {\n  // Hey coder, write your code from here. \n}\n\n console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));",
   },
   {
     id: 9,
@@ -70,7 +70,7 @@ export const questionSet1 = [
     description:
       "Write a function to remove duplicate values from an array using inbuild functions like Set or filter.\n\n For Example:\n\n Input: [1, 2, 2, 3, 4, 4, 5]\n\n Output: [1, 2, 3, 4, 5]",
     starterCode:
-      "function removeDuplicates(arr) {\n  // Hey coader, write your code from here. \n}\n\n console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));",
+      "function removeDuplicates(arr) {\n  // Hey coder, write your code from here. \n}\n\n console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));",
   },
   {
     id: 10,
@@ -79,6 +79,6 @@ export const questionSet1 = [
     description:
       `Write a function to check whether two strings are anagrams without using inbuild JavaScript functions.\n\n An anagram is when two strings contain the same characters in the same frequency, but possibly in a different order.\n\n\n Note: anagrams is those string with both of has same length and same charactor uses in both, places can be diffrent but not any other characher which not contain both if present then considered as not anagrams \n\n For Example:\n\n Input: 'listen', 'silent'\n\n Output: true\n\n Input: 'hello', 'world'\n\n Output: false`,
     starterCode:
-      "function isAnagram(str1, str2) {\n  // Hey coader, write your code from here. \n}\n\n isAnagram('listen', 'silent');",
+      "function isAnagram(str1, str2) {\n  // Hey coder, write your code from here. \n}\n\n console.log(isAnagram('listen', 'silent'));",
   }
 ];
--- src/constants/QuestionBank/interViewQuestion/questionSet2.ts
+++ src/constants/QuestionBank/interViewQuestion/questionSet2.ts
@@ -5,7 +5,7 @@ export const questionSet2 = [
     description:
       "Write a function that returns a Promise which resolves after 2 seconds with the message 'Promise resolved!'. Then, use `.then()` `catch` and `finally` to log the resolved message.\n\nFor Example:\n\nExpected Output (after 2 seconds):\n\n'Promise resolved!'",
     starterCode:
-      "function createDelayedPromise() {\n  // Hey coader, write your code from here.\n}\n\n// Call the function and handle the resolved value.\ncreateDelayedPromise() \n //You have to write (then catch and finally).;",
+      "function createDelayedPromise() {\n  // Hey coder, write your code from here.\n}\n\n// Call the function and handle the resolved value.\n console.log(createDelayedPromise()); \n //You have to write (then catch and finally).;",
   },
   {
     id: 2,
@@ -13,6 +13,6 @@ export const questionSet2 = [
     description:
       "Write a function to demonstrate the difference between shallow copy and deep copy in JavaScript using a nested object.\n\n👉 In shallow copy, changes to nested properties affect the original object.\n👉 In deep copy, the original object remains unchanged when the copy is modified.\n\nFor Example:\n\nInput:\nconst original = {\n  name: 'Nitish',\n  address: { city: 'Delhi' }\n};\n\nExpected Output:\n// After modifying the shallow copy:\noriginal.address.city → 'Mumbai' ❌\n\n// After modifying the deep copy:\noriginal.address.city → 'Delhi' ✅",
     starterCode:
-      "// Hey coader, write your code from here.\n\nconst original = {\n  name: 'Nitish',\n  address: { city: 'Delhi' }\n};\n\n// Create shallow copy and deep copy below\n\n// Perform changes and log the result",
+      "// Hey coder, write your code from here.\n\nconst original = {\n  name: 'Nitish',\n  address: { city: 'Delhi' }\n};\n\n// Create shallow copy and deep copy below\n\n// Perform changes and log the result",
   },
 ];
