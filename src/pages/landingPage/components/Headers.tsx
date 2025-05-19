f7734adc21356d42df9b3f6d4590b1807008280b
nitish <nitishlovecode@gmail.com>
2025-05-19 20:29:14 +0530

    changes made in logo

--- src/pages/landingPage/components/Headers.tsx
+++ src/pages/landingPage/components/Headers.tsx
@@ -1,13 +1,16 @@
-import logo from "../../../assets/logo/ilovedsalogo.png";
+import logo from "../../../assets/logo/colorLogo.png";
+import heartImage from "../../../assets/logo/heartImg.png";
 
 
 function Headers() {
   return (
     <div className="flex items-center p-2 justify-between">
         {/* left div */}
-        <div className="background-color-primary relative">
+        <div className="relative">
             <img className='h-14' src={logo} alt="i love dsa" />
-            <div style={{fontSize: "42px"}} className="absolute -top-2 left-2 animate-bounce  ">❤️</div>
+            <div style={{fontSize: "42px"}} className="absolute -top-2 left-2  ">
+              <img className="h-10 ml-2 mt-3 animate-bounce" src={heartImage} alt="i love dsa.com" />
+            </div>
         </div>
         {/* right div */}
         <div className="border-2 flex p-3 px-6 gap-8 rounded-2xl border-gray-600 w-auto">
